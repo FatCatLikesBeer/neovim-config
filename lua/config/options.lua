f913fb621938f49fc6a55a7f0e4e74249b583ee2
Billy Pamanian <itisbilly@gmail.com>
2024-11-20 19:19:38 -0800

    Updated scrollview and auto indent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Scroll View Settings
+vim.opt.scrolloff = 8
+vim.wo.wrap = true
+
+-- An attempt to get auto indent to work
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
