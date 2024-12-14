c669ecb67b2cde0c479c3944e8195139ff418f20
Billy Pamanian <itisbilly@gmail.com>
2024-12-13 23:50:10 -0800

    Disabled the snacks cursor/scrolling animations

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Turn off the stupid cursor/scrolling animations caused by the snacks plugin
+vim.g.snacks_animate = false
