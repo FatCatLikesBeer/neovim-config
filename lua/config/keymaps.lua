b809e50d1515a16b9719d74080d64a144e7619d2
Billy Pamanian <itisbilly@gmail.com>
2024-12-16 18:22:03 -0800

    Added '<leader> H' as 'go to definition' command

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, { desc = "LSP Hover" })
-vim.keymap.set("i", "<C-h>", vim.lsp.buf.hover, { desc = "LSP Hover" })
+vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, { desc = "LSP Hover" }) -- added: November?
+vim.keymap.set("i", "<C-h>", vim.lsp.buf.hover, { desc = "LSP Hover" }) -- added: November?
+vim.keymap.set("n", "<leader>H", vim.lsp.buf.definition, { desc = "LSP Definition" })
