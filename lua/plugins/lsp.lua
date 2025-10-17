a927b068ff07c282235fa5af6ebc478abb230b2d
Billy <itisbilly@gmail.com>
2025-10-17 01:17:51 -0700

    Attemping to config LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,11 +24,31 @@ return {
           cmd = { "sourcekit-lsp" },
           filetypes = { "swift" },
         },
+        -- PHP stuff
         intelephense = {
           root_dir = function()
             return vim.fn.getcwd()
           end,
         },
+        -- -- TypeScript && JavaScript stuff
+        -- denols = {
+        --   root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc", ".git"),
+        --   init_options = {
+        --     lint = true,
+        --     unstable = true,
+        --   },
+        -- },
+        -- ts_ls = {
+        --   root_dir = function(fname)
+        --     local util = require("lspconfig.util")
+        --     local deno_root = util.root_pattern("deno.json", "deno.jsonc")(fname)
+        --     if deno_root then
+        --       return nil -- Skip tsserver if deno config is found
+        --     end
+        --     return vim.fn.getcwd() -- fallback to single file support
+        --   end,
+        --   single_file_support = true,
+        -- },
       },
     },
   },
